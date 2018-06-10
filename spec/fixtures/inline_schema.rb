effc92ffec67d8b0433c0a456a3b0fbda31cb33c
Kohei Yamamoto <kymmt90@gmail.com>
2018-06-10 19:55:51 +0900

    Add schema fixtures with graphql-ruby's class-based API

--- spec/fixtures/inline_schema.rb
+++ spec/fixtures/inline_schema.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Inline
+  # Schema in legacy-style API
   PostType = GraphQL::ObjectType.define do
     name "Post"
     guard ->(_post, _args, ctx) { ctx[:current_user].admin? }
@@ -30,6 +31,45 @@ module Inline
 
   SchemaWithoutExceptions = GraphQL::Schema.define do
     query QueryType
+    use GraphQL::Guard.new(not_authorized: ->(type, field) {
+      GraphQL::ExecutionError.new("Not authorized to access #{type}.#{field}")
+    })
+  end
+
+  # Schema in class-based API
+  class ClassBasedPost < GraphQL::Schema::Object
+    guard ->(_post, _args, ctx) { ctx[:current_user].admin? }
+    field :id, ID, null: false
+    field :title, String, null: true
+  end
+
+  class ClassBasedQuery < GraphQL::Schema::Object
+    field :posts, [ClassBasedPost], null: false do
+      argument :user_id, ID, required: true
+      guard ->(_obj, args, ctx) { args[:userId] == ctx[:current_user].id }
+    end
+
+    def posts(user_id:)
+      Post.where(user_id: user_id)
+    end
+
+    field :posts_with_mask, [ClassBasedPost], null: false do
+      argument :user_id, ID, required: true
+      mask ->(ctx) { ctx[:current_user].admin? }
+    end
+
+    def posts_with_mask(user_id:)
+      Post.where(user_id: user_id)
+    end
+  end
+
+  class ClassBasedSchema < GraphQL::Schema
+    query ClassBasedQuery
+    use GraphQL::Guard.new
+  end
+
+  class ClassBasedSchemaWithoutExceptions < GraphQL::Schema
+    query ClassBasedQuery
     use GraphQL::Guard.new(not_authorized: ->(type, field) {
       GraphQL::ExecutionError.new("Not authorized to access #{type}.#{field}")
     })
